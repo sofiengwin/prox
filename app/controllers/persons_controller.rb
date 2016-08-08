678f3b62742c5f29051e90eadd1fa163068ffe1d
Ogbara Godwin <godwin.onisofien@andela.com>
2016-08-08 19:40:01 +0100

    feat(person_controller): add get_persons endpoint

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -46,10 +46,17 @@ class PersonsController < ApplicationController
     end
   end
 
+#list of persons in person group
   def list_persons
     uri = "persongroups/#{params[:group]}/persons"
     response = json(make_api_call(uri, :get))
     render json: response
+  end
+
+# list of persons in our db
+  def get_persons
+    persons = Person.all
+    render json: persons
   end
 
   def get_person
